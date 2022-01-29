64bb0424ae5fb20510c4050df419f0c11b859654
etosolo322 <etosolo322@yandex.ru>
2022-01-29 17:26:49 +0300

    master

--- public/js/testform.js
+++ public/js/testform.js
@@ -1,10 +1,10 @@
 document.querySelector('#progressbar')
 let progres = 100;
-/*
+
 setInterval(()=>{
-    document.querySelector('#progressbar').style.height=`${progres-=10}%`;
+    document.querySelector('#progressbar').style.opacity=`${progres-=10}%`;
  
 },1000)
-*/
+
 
 console.log(document.querySelector('#selectA').style)
